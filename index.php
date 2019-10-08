6f8f9f8e7cc42d2008792049f6b6da6f096ff0df
Fakultet Organizacionih Nauka <student1@prod.fon.bg.ac.rs>
2019-10-08 12:37:53 +0200

    Created server array

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
     $serverski_niz = [
-        'ime servera' => $_SERVER['SERVER_NAME']
+        'ime servera' => $_SERVER['SERVER_NAME'],
+        'softver servera' => $_SERVER['SERVER_SOFTWARE']
     ];
 
-    echo $serverski_niz['ime servera'];
+    /*echo $serverski_niz['ime servera'];
+    echo '<br>';
+    echo $serverski_niz['softver servera'];*/
+
+    foreach($serverski_niz as $kljuc => $vrednost){
+        echo $kljuc.":".$vrednost."<br>";
+    }
+
+    print_r($serverski_niz);
+
 ?>
