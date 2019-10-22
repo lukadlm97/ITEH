da3b5b7a16548cbf7c6f463905dd14c93e065efc
Fakultet Organizacionih Nauka <student1@prod.fon.bg.ac.rs>
2019-10-22 13:44:35 +0200

    Sredjeno skoro sve, greska kod insert-a

--- cetvrteVezbe/db.php
+++ cetvrteVezbe/db.php
@@ -6,8 +6,11 @@ class Database{
     private $username="root";
     private $password="";
     private $db_name="bazanovosti";
+    private $result;
+    private $returnet_rows;
+    private $returned_rows_u;
 
-    function connect(){
+    private function connect(){
         $this->connection = new mysqli($this->host,$this->username,$this->password,$this->db_name);
 
         if($this->connection->connect_errno != 0){
@@ -17,4 +20,43 @@ class Database{
         }
 
     }
+
+    function getResult(){
+        return $this->result;
+    }
+
+    function __construct($db_name)
+    {
+        $this->db_name = $db_name;
+        $this->connect();
+    }
+
+    function select($table_name="novosti",$column="naslov,tekst",$condition=null,$sort=null){
+        // upit 
+        $upit = "SELECT $column FROM $table_name ";
+        if($condition != null){
+            $upit .= " WHERE $uslov ";
+        }elseif($sort != null){
+            $upit .= " ORDER BY $sort";
+        }
+        //izvrsi upit
+        $this->executeQuery($upit);
+    }
+
+    function executeQuery($upit){
+        if($this->result = $this->connection->query($upit)){
+            $this->returnet_rows = $this->result->num_rows;
+            $this->returned_rows_u = $this->connection->affected_rows;
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    function insert($podaci,$table_name="novosti",$column="naslov,tekst"){
+        $upit = "INSERT INTO $table_name($column) VALUES($podaci[0],$podaci[1])";
+        return $this->executeQuery($upit);
+    }
+
 }
--- cetvrteVezbe/index.php
+++ cetvrteVezbe/index.php
@@ -1,7 +1,7 @@
 <?php
     include "db.php";
-    $database = new Database();
-    $database->connect();
+    $database = new Database("bazanovosti");
+    
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +13,76 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div>
+        <ul>
+            <li>
+                <a href="index.php?stranica=select"> SELECT </a>
+            </li>
+            <li>
+                <a href="index.php?stranica=insert"> INSERT </a>
+            </li>
+            <li>
+                <a href="index.php?stranica=update"> UPDATE </a>
+            </li>
+            <li>
+                <a href="index.php?stranica=delete"> DELETE </a>
+            </li>
+        </ul>
+
+        <?php
+            if(isset($_GET["stranica"])){
+                $stranica = $_GET["stranica"];
+
+                switch($stranica){
+                    case "select":
+                    //TODO: kreiranje metode select i prikazivanje na stranici
+                    $database->select();
+                    ?>
+                    
+                    <div>
+                        <?php
+                            while($row = $database->getResult()->fetch_object()){
+                                    echo "<h2>$row->naslov</h2>";
+                                    echo "<p>$row->tekst</p>";
+                                    echo "hr";
+                            }
+                        ?>
+                    </div>
+                            <?php
+                                break;
+                                case "insert";
+                                    if(isset($_POST['insert'])){
+                                        $podaci = array($_POST["naslov"],$_POST["tekst"]);
+                                        if($database->insert($podaci)){
+                                            echo "Uspesno dodato!";
+                                        }else{
+                                            echo "Neuspesno dodato!";
+                                        }
+                                        //TODO: jel uspesno
+                                        
+                                    }
+                            ?>
+                            <form action="index.php?stranica=insert" method="POST">
+                                    Naslov:
+                                    <br>
+                                    <input type="text" name="naslov">
+                                    <br>
+                                    Tekst:
+                                    <br>
+                                    <input name="text" type="textarea">
+                                    <br>
+                                    <button type="submit" name="insert">INSERT</button>
+                                    <br>
+                            </form>
+
+                            <?php
+
+                            ?>
+                    <?php
+                    break;
+                }
+            }
+         ?>
+    </div>
 </body>
 </html>
